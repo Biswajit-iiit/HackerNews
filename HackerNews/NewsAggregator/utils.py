fc33cff021e8e1a7f8cda31fffcee5d2e80eef2f
Biswajit Sahu <biswajit@happay.in>
2016-08-09 12:11:07 +0530

    special character fixes

--- HackerNews/NewsAggregator/utils.py
+++ HackerNews/NewsAggregator/utils.py
@@ -9,9 +9,9 @@ def render_result(request,article_dict):
                 'news.html',
                 article_dict,
                 content_type='text/html'))
-        x = s.replace("u&#39;", "'")  # removing unicode issues in JS
-        y = x.replace("&#39;", "'")
-        z = y.replace("Content-Type: text/html", "")
-        p = z.replace("u&quot;","'")
-        q = p.replace("&quot;","'")
+        x = s.replace('u&#39;', '"')  # removing unicode issues in JS
+        y = x.replace('&#39;', '"')
+        z = y.replace('Content-Type: text/html', '')
+        p = z.replace('u&quot;','"')
+        q = p.replace('&quot;','"')
         return q
